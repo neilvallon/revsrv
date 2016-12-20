954713cc0f33ec86096c1b85fa5d742ce04e6048
Neil Vallon <neilvallon@gmail.com>
2016-12-20 02:45:18 -0600

    add custom error pages

--- main.go
+++ main.go
@@ -11,14 +11,6 @@ import (
 	"time"
 )
 
-type comparableHandler struct {
-	http.Handler
-}
-
-var undefinedServer = &comparableHandler{
-	http.NotFoundHandler(),
-}
-
 func main() {
 	aliases := getAliases("alias.csv")
 
@@ -27,39 +19,64 @@ func main() {
 
 		host := aliases.Rewrite(r.Host)
 
-		fs := fileServer(host)
-		if fs == undefinedServer {
+		defer func() {
+			// log request
+			hostRW := r.Host
+			if host != r.Host {
+				hostRW = fmt.Sprintf("%s -> %s", r.Host, host)
+			}
+
+			log.Printf("[%s] %s in %s", hostRW, r.URL.Path, time.Now().Sub(t1))
+		}()
+
+		dir := getRootDir(host)
+		if dir == "" {
 			// wildcard server rewrite
-			fs = fileServer(aliases.Rewrite("*"))
+			defaultDomain := getRootDir(aliases.Rewrite("*"))
+			if defaultDomain == "" {
+				http.Error(w, "Undefined Server", 444)
+			} else {
+				http.FileServer(defaultDomain).ServeHTTP(w, r)
+			}
+			return
 		}
 
-		fs.ServeHTTP(w, r)
+		// custom error rewrites
+		filePath := path.Join(string(dir), path.Clean("/"+r.URL.Path))
+		stat, err := os.Stat(filePath)
 
-		// log request
-		hostRW := r.Host
-		if host != r.Host {
-			hostRW = fmt.Sprintf("%s -> %s", r.Host, host)
+		if os.IsNotExist(err) {
+			aliases.Error(w, r, dir, http.StatusNotFound)
+			return
 		}
 
-		log.Printf("[%s] %s in %s", hostRW, r.URL.Path, time.Now().Sub(t1))
+		if stat.IsDir() {
+			if _, err := os.Stat(path.Join(filePath, "/index.html")); os.IsNotExist(err) {
+				aliases.Error(w, r, dir, http.StatusForbidden)
+				return
+			}
+		}
+
+		http.FileServer(dir).ServeHTTP(w, r)
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-func fileServer(host string) *comparableHandler {
+func getRootDir(host string) http.Dir {
 	l := strings.Split(host, ".")
 	parts := make([]string, len(l))
 	for i := range parts {
 		parts[i] = l[len(l)-i-1]
 	}
 
-	root := path.Join("./", strings.Join(parts, "/"))
+	root := path.Join("./", path.Clean("/"+strings.Join(parts, "/")))
 	if f, err := os.Stat(root); os.IsNotExist(err) || !f.IsDir() {
-		return undefinedServer
+		return ""
 	}
 
-	return &comparableHandler{http.FileServer(http.Dir(root))}
+	dir := http.Dir(root)
+	return dir
 }
 
 type aliasSet map[string]string
@@ -92,4 +109,39 @@ func (a aliasSet) Rewrite(host string) string {
 		host = to
 	}
 	return host
+}
+
+func (a aliasSet) Error(w http.ResponseWriter, r *http.Request, dir http.Dir, code int) {
+	var codeText string
+	switch code {
+	case http.StatusNotFound:
+		codeText = "[404]"
+	case http.StatusForbidden:
+		codeText = "[403]"
+	default:
+		codeText = "[000]"
+	}
+
+	if fileName := a.Rewrite(codeText); fileName == codeText {
+		http.Error(w, http.StatusText(code), code)
+		return
+	} else {
+		// got custom error
+		r.URL.Path = fileName
+
+		f, err := dir.Open(fileName)
+		if err != nil {
+			http.Error(w, http.StatusText(code), code)
+			return
+		}
+		defer f.Close()
+
+		stat, err := f.Stat()
+		if err != nil {
+			http.Error(w, http.StatusText(code), code)
+			return
+		}
+
+		http.ServeContent(w, r, fileName, stat.ModTime(), f)
+	}
 }
